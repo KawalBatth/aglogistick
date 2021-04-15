59f549c8edac36679dbcfb0e374b6096823ba8f8
psjaat <pankajsaharan9@gmail.com>
2021-04-15 20:26:03 +0530

    upate customer

--- application/views/admin/customers/customer_add.php
+++ application/views/admin/customers/customer_add.php
@@ -60,10 +60,7 @@
         <h5 class="modal-title" id="exampleModalLabel">View Rate Sheet</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="caption b17 text-center">
+        </button7="caption b17 text-center">
         Rate sheet for: Road Express
        
     </div>
